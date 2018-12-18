3ea358a114bec7b75cd5520586fecbc227cb394e
Marlon Lira <marlonlira3@gmail.com>
2018-12-18 02:31:42 -0300

    Criação e atualizações de algumas classes

---
+++ Model.Dao/Connection.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.SqlClient;
+
+namespace Model.Dao
+{
+    class Connection
+    {
+        private static Connection ObjConnection = null;
+        private SqlConnection Con;
+
+        private Connection()
+        {
+            Con = new SqlConnection("Data Source=.; Initial Catalog = Admin; Integrated Security = True");
+        }
+        public static Connection ConnectionState()
+        {
+            if (ObjConnection == null)
+            {
+                ObjConnection = new Connection();
+            }
+            return ObjConnection;
+        }
+
+        public SqlConnection GetCon()
+        {
+            return Con;
+        }
+
+        public void CloseConnection()
+        {
+            ObjConnection = null;
+        }
+    }
+}
